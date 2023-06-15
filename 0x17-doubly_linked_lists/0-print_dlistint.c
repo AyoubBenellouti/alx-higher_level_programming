d8af02549d67d9e8659ac19e1d517d45fdc3427c
ayoub benellouti <ayoubbenllouti@gmail.com>
2023-06-15 06:33:51 -0700

    hot fix

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,15 +1,25 @@
-#!/usr/bin/python3
-class DListNode:
-    def __init__(self, value):
-        self.n = value
-        self.prev = None
-        self.next = Nonenode1 = DListNode(1)
-node2 = DListNode(2)
-node3 = DListNode(3)
+#include <stdio.h>
+#include "lists.h"
 
-node1.next = node2
-node2.prev = node1
-node2.next = node3
-node3.prev = node2
-head = node1
-print_dlistint(head)
+int main(void)
+{
+    dlistint_t node1, node2, node3;
+    dlistint_t *head = &node1;
+
+    node1.n = 1;
+    node1.prev = NULL;
+    node1.next = &node2;
+
+    node2.n = 2;
+    node2.prev = &node1;
+    node2.next = &node3;
+
+    node3.n = 3;
+    node3.prev = &node2;
+    node3.next = NULL;
+
+    size_t node_count = print_dlistint(head);
+    printf("Number of nodes: %zu\n", node_count);
+
+    return 0;
+}
